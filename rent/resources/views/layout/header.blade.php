9303992f15f19433addd1031da9b18863d35b0f9
RaghadNaser-1 <126399047+RaghadNaser-1@users.noreply.github.com>
2023-06-01 14:13:53 -0700

    hello

--- rent/resources/views/layout/header.blade.php
+++ rent/resources/views/layout/header.blade.php
@@ -50,7 +50,7 @@
                      </li>
                      <li class="nav-item">
                         <a class="nav-link" href="{{ route('vehicle') }}">Vehicles</a>
-                        <a class="nav-link" href="{{ route('gallery') }}">Gallery</a>
+                        {{-- <a class="nav-link" href="{{ route('gallery') }}">Gallery</a> --}}
                      </li>
                      <li class="nav-item">
                         <a class="nav-link" href="{{ route('client') }}">Client</a>
