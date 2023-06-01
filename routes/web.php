441d31e5d6260194ccf8e18ffecbe75d74799264
TaqwaAlkhlefat <126411392+TaqwaAlkhlefat@users.noreply.github.com>
2023-06-01 14:25:39 +0300

    Taqwa

---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LessorController;
+use App\Http\Controllers\PropertyController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+
+
+
+// Home Page
+Route::get('/', function () {
+    return view('index');
+})->name('home');
+
+// About Page
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+// Services Page
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
+// Vehicles Page
+Route::get('/gallery', function () {
+    return view('gallery');
+})->name('gallery');
+
+// Client Page
+Route::get('/client', function () {
+    return view('client');
+})->name('client');
+
+// Contact Page
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+// lessor Page
+// Route::get('/lessor', function () {
+//     return view('lessor.index');
+// })->name('lessor.index');
+
+
+// Route::get('/lessor', 'LessorController@index')->name('lessor.index');
+Route::get('/lessor', [LessorController::class, 'index'])->name('lessor.index');
+
+Route::put('/lessors/{lessor}', [LessorController::class, 'update'])->name('lessor.update');
+
+Route::get('/property/create', [PropertyController::class, 'create'])->name('property.create');
+Route::post('/property', [PropertyController::class, 'store'])->name('property.store');
+
+
+
+
